be2091765fe65cc18d08d5f59a4805ce5fa7a1ba
John Lorenze Panaligan <john.panaligan@awsys-i.com>
2018-08-13 18:13:15 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
